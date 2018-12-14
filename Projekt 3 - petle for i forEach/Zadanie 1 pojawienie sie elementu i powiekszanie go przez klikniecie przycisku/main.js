a118ff8e87f02021754829d7e13718bd9054fc11
Mikołaj Lechtański <mikolec@gmail.com>
2018-12-14 17:48:59 +0100

    Projekt 3 - petle for i forEach - zadanie 2 - moje rozwiązanie

--- Projekt 3 - petle for i forEach/Zadanie 1 pojawienie sie elementu i powiekszanie go przez klikniecie przycisku/main.js
+++ Projekt 3 - petle for i forEach/Zadanie 1 pojawienie sie elementu i powiekszanie go przez klikniecie przycisku/main.js
@@ -2,18 +2,17 @@ const btn = document.querySelector("button"),
       lis = document.querySelectorAll("li");
 let fontSize = 10;
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", () => {
 
-    // for (let i=0; i<lis.length; i++) {
+    // for (let i=0; i<lis.length; i2++) {
     //     lis[i].style.fontSize = fontSize + "px";
     //     lis[i].style.display = "block";
     // }
 
-    lis.forEach(function(li){
-        li.style.fontSize = fontSize + "px";
+    lis.forEach(li => {
+        li.style.fontSize = `${fontSize}px`;
         li.style.display = "block";
     });
-
+    
     fontSize++;
 });
-
---
+++ Projekt 3 - petle for i forEach/Zadanie 2 tworzenie listy i dodawanie kolejnych elementow przez klikniecie/main.js
@@ -0,0 +1,57 @@
+/**
+ * - Stwórz rozwiązanie z pomocą dwóch funkcji w oparciu o strukturę w main.js Nie dodawaj nic więcej do zakresu globalnego.
+
+- funkcja init, która będzie uruchomiona po wczytaniu strony. Jej zadaniem jest:
+--- stworzyć dwa elementy
+<button>Dodaj 10 elementów listy</buttn>
+<ul></ul>
+--- dodaj je do body
+--- ustaw nasłuchiwanie na przycisk
+
+
+--- funkcja createLiElements, która będzie uruchamiana po kliknięciu przycisku stworzonego przez funkcję init. Jej zadanie to:
+--- tworzenie 10 elementów li i umieszczanie ich w elemencie ul
+--- kazdy z 10 elementów ma mieć swój indeks (order) pczy czym kolejne 10 elementów ma być konynuacją (czilu pierwszy klik mamy 1-10, kolejny 11-20 itd)
+--- każdy kolejny element il ma mieć font-size większy o 1px.
+
+ */
+
+let size = 10;
+let orderElement = 1;
+
+const init = () => {
+
+    const btnAdd = document.createElement("button"),
+        btnReset = document.createElement("button"),
+        ul = document.createElement("ul");
+    
+    btnAdd.textContent = "Dodaj 10 elementów listy";
+    btnReset.textContent = "Resetuj";
+    document.body.appendChild(btnAdd);
+    document.body.appendChild(btnReset);
+    document.body.appendChild(ul);
+    btnAdd.addEventListener("click", createLiElements);
+    btnReset.addEventListener("click", () => {
+        // ul.parentNode.removeChild(ul);
+        document.querySelectorAll("li").forEach((li) => {
+            li.remove();
+        });
+        size = 10;
+        orderElement = 1; 
+    });
+}
+
+const createLiElements = () => {
+
+    for(let i = 0; i < 10; i++) {
+        const li = document.createElement("li");
+        li.textContent = `Element nr ${orderElement}`;
+        li.style.fontSize = `${size}px`
+        document.querySelector("ul").appendChild(li);
+        orderElement++;
+        size++;
+    }
+
+}
+
+init()
