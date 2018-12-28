cc9dd0ae67e4b3ecb67d35dd0143ea907b68d4b2
Mikołaj Lechtański <mikolec@gmail.com>
2018-12-28 19:51:34 +0100

    Projekt 11 - wyświetlanie aktualnej daty i godziny - zadanie 2 - odliczanie czasu

--- Projekt 11 - zegar i odliczanie/Zadanie 2 - odliczanie czasu do jakiegos wydarzenia/main.js
+++ Projekt 11 - zegar i odliczanie/Zadanie 2 - odliczanie czasu do jakiegos wydarzenia/main.js
@@ -1,27 +1,25 @@
-const endTime = new Date('2019-01-05 18:34:00').getTime();
+//const nowTime = new Date().getTime(); //data początkowa; getTime - wartość w milisekundach
+const endTime = new Date("2019-01-25 22:34:00").getTime();
+//const time = endTime - nowTime;
 
-const spanD = document.querySelector('span.d');
-const spanH = document.querySelector('span.h');
-const spanM = document.querySelector('span.m');
-const spanS = document.querySelector('span.s');
+const spanD = document.querySelector("span.d");
+const spanH = document.querySelector("span.h");
+const spanM = document.querySelector("span.m");
+const spanS = document.querySelector("span.s");
 
 setInterval(() => {
- const nowTime = new Date().getTime();
- // const time = Math.floor((endTime - nowTime) / 1000);
- const time = endTime - nowTime;
- const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
- console.log(days);
+  const nowTime = new Date().getTime();
+  const time = endTime - nowTime;
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+  hours = hours < 10 ? "0" + hours : hours;
+  let minutes = Math.floor((time / (1000 * 60)) % 60);
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let seconds = Math.floor((time / 1000) % 60);
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
- let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
- // Przykład - dodanie 0 przeg godziną 
- hours = hours < 10 ? `0${hours}` : hours;
-
- const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
-
- const secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
-
- spanD.textContent = days;
- spanH.textContent = hours;
- spanM.textContent = minutes;
- spanS.textContent = secs;
-}, 1000)
+  spanD.textContent = days;
+  spanH.textContent = hours;
+  spanM.textContent = minutes;
+  spanS.textContent = seconds;
+});
