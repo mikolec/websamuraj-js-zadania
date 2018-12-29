638aba4d6c13ac61368c3f53d5e478711ff5ac6e
Mikołaj Lechtański <mikolec@gmail.com>
2018-12-29 11:18:51 +0100

    Projekt 11 - wyświetlanie aktualnej daty i godziny - zadanie 3 - moje rozwiązanie - v.setTimeout

--- Projekt 11 - zegar i odliczanie/Zadanie 3 - stoper/main.js
+++ Projekt 11 - zegar i odliczanie/Zadanie 3 - stoper/main.js
@@ -5,24 +5,31 @@ const resetBtn = document.querySelector("button.reset");
 const display = document.querySelector(".time div");
 
 let counter = 0;
+let active = false;
 
 const timer = () => {
-  counter++;
-  let s = Math.floor(counter / 100);
-  let ss = Math.floor(counter % 100);
-  ss = ss < 10 ? "0" + ss : ss;
-  let timeStr = `${s}.${ss}`;
-  display.textContent = timeStr;
+  if (active) {
+    counter++;
+    let s = Math.floor(counter / 100);
+    let ss = Math.floor(counter % 100);
+    ss = ss < 10 ? "0" + ss : ss;
+    let timeStr = `${s}.${ss}`;
+    display.textContent = timeStr;
+    setTimeout(timer, 10);
+  }
 };
 
 let timerIndex = null;
 
 mainBtn.addEventListener("click", () => {
   if (mainBtn.textContent === "Start") {
-    timerIndex = setInterval(timer, 10);
+    //timerIndex = setInterval(timer, 10);
+    active = true;
+    timer();
     mainBtn.textContent = "Pauza";
   } else if (mainBtn.textContent === "Pauza") {
-    clearInterval(timerIndex);
+    //clearInterval(timerIndex);
+    active = false;
     mainBtn.textContent = "Start";
   }
 });
@@ -31,4 +38,5 @@ resetBtn.addEventListener("click", () => {
   counter = 0;
   mainBtn.textContent = "Start";
   display.textContent = "---";
+  active = false;
 });
